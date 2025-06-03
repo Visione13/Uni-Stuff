e286cbe6738bfe054257815f66077575bf951250
Visione <marcel-gruenheid@gmx.de>
2025-06-03 13:53:11 +0200

    Pk1 Übung generics

--- PK1/Uebung/generic_collection/src/Main.java
+++ PK1/Uebung/generic_collection/src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         AkzeptablesRisiko ar = new AkzeptablesRisiko("Lizenzkosten der IDE steigt",100,40);
-        ExtremesRisiko er = new ExtremesRisiko("Hauptauftraggeber meldet Insolvenz an", 1, 50000, "Versicherung abschließen", 50000);
+        ExtremesRisiko er = new ExtremesRisiko("Hauptauftraggeber meldet Insolvenz an", 1, 60, "Versicherung abschließen", 500);
         InakzeptablesRisiko ir = new InakzeptablesRisiko("DB Experte verlässt das Projekt", 50, 320,  "Ersatz bei Dienstleister reservieren");
-        ar.druckeDaten();
-        er.druckeDaten();
-        ir.druckeDaten();
-        ar.equals(er);
+
+        Risikoverwaltung rv = new Risikoverwaltung();
+        rv.aufnehmen(ar);
+        rv.aufnehmen(er);
+        rv.aufnehmen(ir);
+
+        rv.zeigeRisiken();
+
+        rv.sucheRisikoMitMaxRueckstellung();
+
+        rv.berechneSummeRueckstellungen();
     }
 }
--- PK1/Uebung/generic_collection/src/Risikoverwaltung.java
+++ PK1/Uebung/generic_collection/src/Risikoverwaltung.java
@@ -1,14 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Risikoverwaltung {
-    public void aufnehmen(){
+    List<Risiko> risiken = new ArrayList<Risiko>();
 
+    public void aufnehmen(Risiko risiko){
+        risiken.add(risiko);
     }
+
     public void zeigeRisiken(){
-
+        for(Risiko risiko : risiken){
+            System.out.println("ID: " + risiko.getId() + " Bezeichnung: " + risiko.getBezeichnung() + " Rückstellung: " + risiko.ermittleRueckstellung());
+        }
     }
+
     public void sucheRisikoMitMaxRueckstellung(){
-
+        float max = 0;
+        Risiko maxRisiko = null;
+        for (Risiko risiko : risiken) {
+            if(risiko.ermittleRueckstellung() > max){
+                max = risiko.ermittleRueckstellung();
+                maxRisiko = risiko;
+            }
+        }
+        if(maxRisiko != null){
+            System.out.println("ID: " + maxRisiko.getId() + " Bezeichnung: " + maxRisiko.getBezeichnung() + " Rückstellung: " + maxRisiko.ermittleRueckstellung());
+        }
     }
-    public void berechneSummeRueckstellungen(){
 
+    public void berechneSummeRueckstellungen(){
+        float sum = 0;
+        for (Risiko risiko : risiken) {
+            sum += risiko.ermittleRueckstellung();
+        }
+        System.out.println("Summe: " + sum);
     }
 }
