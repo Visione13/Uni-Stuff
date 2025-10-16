57bd01b66620a226ad0a2c86b2fdff8da824ddcb
alice <agruenheid@pm.me>
2025-10-16 06:19:19 +0200

    Übung 2 EidP

---
+++ EidP/Uebung 2/src/AdditionMitMatrNr.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AdditionMitMatrNr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        final int nummer = 1234567;
+        System.out.println(nummer + x + y);
+    }
+}
